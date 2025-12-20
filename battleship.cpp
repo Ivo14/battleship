c2eee3f6f4c12355abe802b760ed198aa3f394e6
Ivo14 <70132844+Ivo14@users.noreply.github.com>
2025-12-20 20:54:45 +0200

    3
    automatic set up; cases

--- battleship.cpp
+++ battleship.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <windows.h>
 using namespace std;
 const size_t MAX = 15;
 const char water = '~';
@@ -15,6 +16,7 @@ const int destroyers = 4;
 const int submarines = 3;
 const int cruisers = 2;
 const int carriers = 1;
+const char *typeBoat[] = {"Destroyer", "Submarine", "Cruiser", "Carrier"};
 
 int absoluteValue(int a)
 {
@@ -90,9 +92,11 @@ void fillInBoatPositions(char board[MAX][MAX], const size_t size, const int amou
         while (incorrectPlacement)
         {
             incorrectPlacement = false;
-            cout << "\nSubmarine Start Position: ";
+            cout << "\n"
+                 << typeBoat[boatSize - 1] << " Start Position: ";
             cin >> startX >> startY;
-            cout << "\nSubmarine End Position: ";
+            cout << "\n"
+                 << typeBoat[boatSize - 1] << " End Position: ";
             cin >> endX >> endY;
 
             if (!areValidCoordinates(startX, startY, endX, endY, boatSize, size))
@@ -100,14 +104,17 @@ void fillInBoatPositions(char board[MAX][MAX], const size_t size, const int amou
                 incorrectPlacement = true;
                 cout << "Try again with new coordinates. These are not valid.";
             }
-            if (!incorrectPlacement && !areOccupied(board, startX, startY, endX, endY))
-            {
-                incorrectPlacement = false;
-            }
             else
             {
-                incorrectPlacement = true;
-                cout << "Try again with new coordinates. These are already occupied.";
+                if (!incorrectPlacement && !areOccupied(board, startX, startY, endX, endY))
+                {
+                    incorrectPlacement = false;
+                }
+                else
+                {
+                    incorrectPlacement = true;
+                    cout << "Try again with new coordinates. These are already occupied.";
+                }
             }
         }
 
@@ -125,6 +132,68 @@ void fillInBoatPositions(char board[MAX][MAX], const size_t size, const int amou
                 {
                     board[i][startY - 1] = boat;
                 }
+            }
+        }
+    }
+}
+
+void fillInBoatPositionsAuto(char board[MAX][MAX], const size_t size, const int amount, const int boatSize)
+{
+    for (int t = 0; t < amount; t++)
+    {
+        bool incorrectPlacement = true;
+        int startX;
+        int startY;
+        int endX;
+        int endY;
+        while (incorrectPlacement)
+        {
+            incorrectPlacement = false;
+
+            srand(time(0));
+            int orientation = rand() % 2;
+
+            if (orientation == 0)
+            {
+                startX = (rand() % size) + 1;
+                endX = startX;
+                startY = (rand() % (size - boatSize + 1)) + 1;
+                endY = startY + boatSize - 1;
+            }
+            else
+            {
+                startX = (rand() % (size - boatSize + 1)) + 1;
+                endX = startX + boatSize - 1;
+                startY = (rand() % size) + 1;
+                endY = startY;
+            }
+
+            if (!areValidCoordinates(startX, startY, endX, endY, boatSize, size))
+            {
+                incorrectPlacement = true;
+            }
+            if (!incorrectPlacement && !areOccupied(board, startX, startY, endX, endY))
+            {
+                incorrectPlacement = false;
+            }
+            else
+            {
+                incorrectPlacement = true;
+            }
+        }
+
+        if (startX == endX)
+        {
+            for (int j = startY - 1; j <= endY - 1; j++)
+            {
+                board[startX - 1][j] = boat;
+            }
+        }
+        else
+        {
+            for (int i = startX - 1; i <= endX - 1; i++)
+            {
+                board[i][startY - 1] = boat;
             }
         }
     }
@@ -161,25 +230,58 @@ void printBoards(char firstBoard[MAX][MAX], char secondBoard[MAX][MAX], const si
 
 int main()
 {
-    const size_t size = 15;
-    char computerBoard[size][size];
-    char PlayerBoard[size][size];
-    fillWithWater(computerBoard, size);
+    int type = -1;
+    while (type < 1 || type > 3)
+    {
+        cout << "Enter 1 for Calm Waters (10x10)\nEnter 2 for Rough Seas (12x12)\nEnter 3 for Storm of Steel (15x15)\n";
+        cin >> type;
+    }
+    switch (type)
+    {
+    case 1:
+        type = 10;
+        break;
+    case 2:
+        type = 12;
+        break;
+    case 3:
+        type = 15;
+        break;
+    };
+    const size_t size = type;
+    char computerBoard[MAX][MAX];
+    char computerShowBoard[MAX][MAX];
+    char PlayerBoard[MAX][MAX];
+    fillWithWater(computerShowBoard, size);
     fillWithWater(PlayerBoard, size);
+    fillWithWater(computerBoard, size);
 
+    int automatic = -1;
+    while (automatic < 1 || automatic > 2)
+    {
+        cout << "Enter 1 for automatic arrangment\nEnter 2 to set up the board yourself\n";
+        cin >> automatic;
+    }
+    switch (automatic)
+    {
+    case 1:
+        fillInBoatPositionsAuto(PlayerBoard, size, carriers, carrierSize);
+        fillInBoatPositionsAuto(PlayerBoard, size, cruisers, cruiserSize);
+        fillInBoatPositionsAuto(PlayerBoard, size, submarines, submarineSize);
+        fillInBoatPositionsAuto(PlayerBoard, size, destroyers, destroyerSize);
+        break;
+    case 2:
+        fillInBoatPositions(PlayerBoard, size, destroyers, destroyerSize);
+        fillInBoatPositions(PlayerBoard, size, submarines, submarineSize);
+        fillInBoatPositions(PlayerBoard, size, cruisers, cruiserSize);
+        fillInBoatPositions(PlayerBoard, size, carriers, carrierSize);
+        break;
+    };
+    printBoards(computerShowBoard, PlayerBoard, size);
+    fillInBoatPositionsAuto(computerBoard, size, carriers, carrierSize);
+    fillInBoatPositionsAuto(computerBoard, size, cruisers, cruiserSize);
+    fillInBoatPositionsAuto(computerBoard, size, submarines, submarineSize);
+    fillInBoatPositionsAuto(computerBoard, size, destroyers, destroyerSize);
     printBoards(computerBoard, PlayerBoard, size);
-
-    fillInBoatPositions(PlayerBoard, size, submarines, submarineSize);
-    printBoards(computerBoard, PlayerBoard, size);
-
-    fillInBoatPositions(PlayerBoard, size, cruisers, cruiserSize);
-    printBoards(computerBoard, PlayerBoard, size);
-
-    fillInBoatPositions(PlayerBoard, size, carriers, carrierSize);
-    printBoards(computerBoard, PlayerBoard, size);
-
-    fillInBoatPositions(PlayerBoard, size, destroyers, destroyerSize);
-    printBoards(computerBoard, PlayerBoard, size);
-
     return 0;
 }
